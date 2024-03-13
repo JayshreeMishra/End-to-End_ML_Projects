aa6f607dee07079f5feae9f27fd65b20a2c5603e
Jayshree Mishra <j1shree.in@gmail.com>
2024-03-13 14:50:01 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from red_wine_quality_prediction import logger
+from red_wine_quality_prediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("This is our custom log")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
