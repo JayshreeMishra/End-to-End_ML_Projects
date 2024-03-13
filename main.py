eaebb03e1d948a740a8c8f081a007913f82a6fb3
Jayshree Mishra <j1shree.in@gmail.com>
2024-03-13 15:44:23 +0530

    Data validation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from red_wine_quality_prediction import logger
 from red_wine_quality_prediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from red_wine_quality_prediction.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -12,3 +13,14 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
